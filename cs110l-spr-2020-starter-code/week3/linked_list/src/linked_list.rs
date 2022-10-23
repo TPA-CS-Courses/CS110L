87ce36ed094ed73ade735de03fff719651a94fc7
tianpingan <tianpingan2000@gmail.com>
2022-10-23 08:22:40 +0000

    week3: finished part2

--- cs110l-spr-2020-starter-code/week3/linked_list/src/linked_list.rs
+++ cs110l-spr-2020-starter-code/week3/linked_list/src/linked_list.rs
@@ -1,24 +1,24 @@
 use std::fmt;
 use std::option::Option;
 
-pub struct LinkedList {
-    head: Option<Box<Node>>,
+pub struct LinkedList<T: fmt::Display + std::cmp::PartialEq + Clone> {
+    head: Option<Box<Node<T>>>,
     size: usize,
 }
 
-struct Node {
-    value: u32,
-    next: Option<Box<Node>>,
+struct Node<T: fmt::Display + std::cmp::PartialEq + Clone> {
+    value: T,
+    next: Option<Box<Node<T>>>,
 }
 
-impl Node {
-    pub fn new(value: u32, next: Option<Box<Node>>) -> Node {
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> Node<T> {
+    pub fn new(value: T, next: Option<Box<Node<T>>>) -> Node<T> {
         Node {value: value, next: next}
     }
 }
 
-impl LinkedList {
-    pub fn new() -> LinkedList {
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> LinkedList<T> {
+    pub fn new() -> LinkedList<T> {
         LinkedList {head: None, size: 0}
     }
     
@@ -30,14 +30,14 @@ impl LinkedList {
         self.get_size() == 0
     }
     
-    pub fn push_front(&mut self, value: u32) {
-        let new_node: Box<Node> = Box::new(Node::new(value, self.head.take()));
+    pub fn push_front(&mut self, value: T) {
+        let new_node: Box<Node<T>> = Box::new(Node::new(value, self.head.take()));
         self.head = Some(new_node);
         self.size += 1;
     }
     
-    pub fn pop_front(&mut self) -> Option<u32> {
-        let node: Box<Node> = self.head.take()?;
+    pub fn pop_front(&mut self) -> Option<T> {
+        let node: Box<Node<T>> = self.head.take()?;
         self.head = node.next;
         self.size -= 1;
         Some(node.value)
@@ -45,9 +45,9 @@ impl LinkedList {
 }
 
 
-impl fmt::Display for LinkedList {
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> fmt::Display for LinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut current: &Option<Box<Node>> = &self.head;
+        let mut current: &Option<Box<Node<T>>> = &self.head;
         let mut result = String::new();
         loop {
             match current {
@@ -62,7 +62,7 @@ impl fmt::Display for LinkedList {
     }
 }
 
-impl Drop for LinkedList {
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> Drop for LinkedList<T> {
     fn drop(&mut self) {
         let mut current = self.head.take();
         while let Some(mut node) = current {
@@ -70,6 +70,68 @@ impl Drop for LinkedList {
         }
     }
 }
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        // Self { head: self.head.clone(), size: self.size.clone() }
+        let mut list: LinkedList<T> = LinkedList::new();
+        let mut current: &Option<Box<Node<T>>> = &self.head;
+        loop {
+            match current {
+                Some(node) => {
+                    list.push_front(node.value.clone());
+                    current = &node.next;
+                },
+                None => break,
+            }
+        }
+        list
+    }
+}
 
 
-
+impl<T: fmt::Display + std::cmp::PartialEq + Clone> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        // self.isbn == other.isbn
+        if self.size != other.size {
+            false
+        } else {
+            let mut flag = true;
+            let mut current_self: &Option<Box<Node<T>>> = &self.head;
+            let mut current_other = &self.head;
+            loop {
+                match current_self {
+                    Some(node_self) => {
+                        // result = format!("{} {}", result, node.value);
+                        match current_other {
+                            Some(node_other) => {
+                                if node_other.value != node_self.value {
+                                    flag = false;
+                                    break;
+                                }
+                                current_other = &node_other.next;
+                            }, 
+                            None => {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        
+                        current_self = &node_self.next;
+                    },
+                    None => {
+                        match current_other {
+                            Some(node_other) => {
+                                flag = false;
+                                break;
+                            }, 
+                            None => {
+                                break;
+                            }
+                        }
+                    },
+                }
+            }
+            flag
+        }
+    }
+}
--- cs110l-spr-2020-starter-code/week3/linked_list/src/main.rs
+++ cs110l-spr-2020-starter-code/week3/linked_list/src/main.rs
@@ -2,18 +2,35 @@ use linked_list::LinkedList;
 pub mod linked_list;
 
 fn main() {
-    let mut list: LinkedList<u32> = LinkedList::new();
+    let mut list: LinkedList<String> = LinkedList::new();
     assert!(list.is_empty());
     assert_eq!(list.get_size(), 0);
-    for i in 1..12 {
-        list.push_front(i);
-    }
+    list.push_front(String::from("Hello"));
+    list.push_front(String::from("World"));
+    list.push_front(String::from("Fuck"));
+    list.push_front(String::from("You"));
     println!("{}", list);
     println!("list size: {}", list.get_size());
     println!("top element: {}", list.pop_front().unwrap());
     println!("{}", list);
     println!("size: {}", list.get_size());
     println!("{}", list.to_string()); // ToString impl for anything impl Display
+    let list2 = list;
+    println!("{}", list2);
+    
+    
+    // let mut list: LinkedList<u32> = LinkedList::new();
+    // assert!(list.is_empty());
+    // assert_eq!(list.get_size(), 0);
+    // for i in 1..12 {
+    //     list.push_front(i);
+    // }
+    // println!("{}", list);
+    // println!("list size: {}", list.get_size());
+    // println!("top element: {}", list.pop_front().unwrap());
+    // println!("{}", list);
+    // println!("size: {}", list.get_size());
+    // println!("{}", list.to_string()); // ToString impl for anything impl Display
 
     // If you implement iterator trait:
     //for val in &list {
